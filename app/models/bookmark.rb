85e97ebf42cee39f4b547b467aa6741f93d1b455
Davide Tello <tello.davide@gmail.com>
2021-03-29 17:27:58 +0200

    Validations and associations for the models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
+  # Asscoiations
   belongs_to :movie
   belongs_to :list
+  # Validations
+  validates_uniqueness_of :movie_id, scope: [:list_id]
+  validates :comment, length: { minimum: 6 }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,8 @@
 class List < ApplicationRecord
+  # Associations
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+  # Validations
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
