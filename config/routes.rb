cc4fbb0a39472e7f7050f8d77b862668e06a7916
Davide Tello <tello.davide@gmail.com>
2021-03-30 10:23:23 +0200

    Add Routes for the Bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists, only: %i[show new create]
+  resources :lists, only: %i[show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
 end
