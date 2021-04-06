6842049a9aa06c5db611889bbc8331bd2eccf65a
Davide Tello <tello.davide@gmail.com>
2021-04-06 10:31:47 +0200

    Add Create new Bookmark action

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,19 @@
 class BookmarksController < ApplicationController
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list_id = params[:list_id]
+    @bookmark.movie_id = params[:bookmark][:movie_id]
+
+    if @bookmark.save
+      redirect_to list_path(params[:list_id]), notice: 'Movie was successfully created.'
+    else
+      render 'list/new'
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
 end
