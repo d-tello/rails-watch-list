e96e152f4a18df99973c342c7eeb6bc9affc0bd1
Davide Tello <tello.davide@gmail.com>
2021-04-06 11:19:15 +0200

    Add destroy action for a bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -12,6 +12,12 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(params[:id])
+  end
+
   private
 
   def bookmark_params
